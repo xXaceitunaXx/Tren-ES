fc54e352c5c66aa6cec96f45f6c5d6d7c026ac96
Víctor Elvira Fernández <victorelv04@gmail.com>
2026-03-24 12:39:47 +0100

    Refactor distance condition and clean up SQL queries
    Updated SQL query to consider both directions for distance calculation and removed unnecessary queries.

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -9,22 +9,13 @@ SELECT V.id
 	INNER JOIN Ruta AS R
 		ON V.ruta = R.id
 	INNER JOIN Distancia AS D
-		ON (R.origen = D.estacion1 AND R.destino = D.estacion2) -- Asumimos que construimos la tabla Distancia de forma simétrica
+		ON (R.origen = D.estacion1 AND R.destino = D.estacion2) OR
+		(R.origen = D.estacion2 AND R.destino = D.estacion1)
 	WHERE D.distancia < 30;
-
-
-SELECT V.id, E1.nombre AS estacion_origen, E2.nombre AS estacion_destino, D.distancia
-	FROM Poblaciones AS P
-	INNER JOIN Viajes AS V
-    		ON P.localidad = V.origen
-	WHERE P.poblacion > 20000 AND P.poblacion < 100000
-		AND V.fecha = CURRENT_DATE
-	GROUP BY P.localidad
-	HAVING COUNT(V.id) < 5;
 
 SELECT M.id, M.nombre, COUNT(DISTINCT V.id) AS num_viajes_hoy
 	FROM Municipio AS M
-	LEFT JOIN Estacion AS E -- Utilizamos LEFT JOIN para incluir municipios sin estaciones, paradas o viajes
+	LEFT JOIN Estacion AS E 
 		ON E.municipio = M.id
 	LEFT JOIN Parada AS P
 		ON P.estacion = E.id
