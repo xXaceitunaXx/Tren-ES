68206dc0c89267cc07d8bd838eeb175fcade95bd
juahorr <juan.horrillo22@estudiantes.uva.es>
2026-03-25 09:14:48 +0100

    Creados scripts de despliegue de bases de datos fuentes origen. Refactorizado el script del mediador a un solo fichero con el ciclo completo de creación, inserción y consulta

--- scripts/sql/distancia.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Distancia (
-    estacion1 INT NOT NULL,
-    estacion2 INT NOT NULL,
-    distancia DECIMAL(8,2) NOT NULL,
-    PRIMARY KEY (estacion1, estacion2),
-    FOREIGN KEY (estacion1) REFERENCES Estacion(id),
-    FOREIGN KEY (estacion2) REFERENCES Estacion(id)
-);
--- scripts/sql/viaje.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Viaje (
-    id INT PRIMARY KEY,
-    ruta INT NOT NULL,
-    fecha DATE NOT NULL,
-    horario TIME NOT NULL,
-    FOREIGN KEY (ruta) REFERENCES Ruta(id)
-);
-
